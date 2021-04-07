8111842164e21ea3804df365b61b7ed94ad480fa
Fayas <TheFayasNoushad@gmail.com>
2021-04-07 21:33:29 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -5,7 +5,6 @@ if bool(os.environ.get("WEBHOOK", False)):
 else:
     from config import Config
 
-from plugins.commands import start, help
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
 from translation import Translation
