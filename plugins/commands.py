69398710da57e74de69e8e2d9a29d919c79f4531
Fayas <TheFayasNoushad@gmail.com>
2021-04-25 14:51:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,12 +7,12 @@ from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.command(["start", "help"]) & filters.private)
+@Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.mention),
-        parse_mode="html", disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ JOIN UPDATES CHANNEL ⚙', url='https://telegram.me/FayasNoushad')]]),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_TEXT,
         reply_to_message_id=update.message_id
     )
