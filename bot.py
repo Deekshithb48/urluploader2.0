9a21d15ba912394460f13d7de8cac898ecbcf33f
Fayas <TheFayasNoushad@gmail.com>
2021-04-25 14:52:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = Client(
-        "URL Uploader",
+        "YouTube Downloader",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
