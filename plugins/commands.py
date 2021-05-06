8b8ef7ba20320fc87d5854d986bd6fe777a74280
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-06 12:31:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,6 +13,6 @@ async def start(bot, update):
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup=Translation.START_TEXT,
+        reply_markup=Translation.START_BUTTONS,
         reply_to_message_id=update.message_id
     )
